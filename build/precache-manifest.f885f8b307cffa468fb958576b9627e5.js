11af41fa78b3c1683895d2500e5df3dc75f37123
Abhinav Saini <sainiabhinav75@gmail.com>
2019-07-09 23:07:12 +0530

    React Build Folder added

---
+++ build/precache-manifest.f885f8b307cffa468fb958576b9627e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95b4ed66ae67f5c5610a839a7a56bba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab0d57b026e5ce98133d",
+    "url": "/static/css/main.cbd143d0.chunk.css"
+  },
+  {
+    "revision": "7d5ed4fa89594373c7ef",
+    "url": "/static/js/2.e4fac51a.chunk.js"
+  },
+  {
+    "revision": "ab0d57b026e5ce98133d",
+    "url": "/static/js/main.a799166d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
